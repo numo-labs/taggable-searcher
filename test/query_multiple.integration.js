e137d5d4c5901cd043bef6efc23552061c703449
Jimmy Rutsfrom:(noreply@github.com)Rutsfrom:(noreply@github.com) <jimmy.ruts@thomascookonline.com>
2016-04-12 14:30:50 +0100

    Removed unneeded complexity

--- test/query_multiple.integration.js
+++ test/query_multiple.integration.js
@@ -19,23 +19,4 @@ describe('Integration test for multiple queries', function () {
       });
     });
   });
-  it('should return merge results when chunking', function (done) {
-    index.searchByName(['spain', 'greece'], {idPrefix: 'geo', size: 1}, function (err, data) {
-      if (err) return done(err);
-      assert.equal(data.hits.found, 3);
-      assert.equal(data.hits.hit.length, 1);
-      assert.equal(data.hits.start, 0);
-      index.searchByTags(data.hits.hit[0].id, {idPrefix: 'hotel:mhid'}, function (err, data) {
-        if (err) return done(err);
-        var ids = data.hits.hit.map(function (hit) {
-          return hit.id;
-        });
-        index.searchByTags(ids, {}, function (err, data) {
-          if (err) return done(err);
-          assert.equal(data.hits.start, 0);
-          done();
-        });
-      });
-    });
-  });
 });
