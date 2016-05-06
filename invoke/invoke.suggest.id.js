5a0b4ff7641b07fd291b2a2e77e8978def1d2dc4
Jimmy Rutsfrom:(noreply@github.com)Rutsfrom:(noreply@github.com) <jimmy.ruts@thomascookonline.com>
2016-05-06 11:52:38 +0100

    ids are now accepted

---
+++ invoke/invoke.suggest.id.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var index = require('../index');
+
+var options = {
+  text: 'geo:geonames.2510769',
+  size: 1,
+  include: 'geo',
+  context: 'dk:da'
+};
+
+index.suggest(options, function (err, data) {
+  if (err) console.error(err);
+  console.log(JSON.stringify(data));
+  assert.equal(data.hits.hit[0].fields.name, 'Spanien');
+});
