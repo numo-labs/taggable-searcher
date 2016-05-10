fb2731121a382d26ab2a15cdc1bd4063fe25ceb3
Jimmy Rutsfrom:(noreply@github.com)Rutsfrom:(noreply@github.com) <jimmy.ruts@thomascookonline.com>
2016-05-10 11:10:36 +0100

    Improved the article search for geo

--- invoke/invoke.search.tile.js
+++ invoke/invoke.search.tile.js
@@ -1,13 +1,14 @@
+require('env2')('.env');
 var assert = require('assert');
 var index = require('../index');
 
 var params = {
-  includedIn: ['marketing:concept.casacook', 'geo:geonames.390903', 'amenity:test.1245'],
+  includedIn: ['geo:geonames.390903'],
   limit: 2
 };
 
 index.search.tile(params, function (err, data) {
   if (err) console.error(err);
   console.log(JSON.stringify(data, null, 2));
-  assert.equal(data.length, 1);
+  assert.equal(data.length, 2);
 });
