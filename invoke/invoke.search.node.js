b5a6d7f3b81679034fd07c7bad011c3d62bcab24
Pascal Laenen <plaenen@gmail.com>
2016-05-11 19:25:55 +0100

    added filter capability for tiles

--- invoke/invoke.search.node.js
+++ invoke/invoke.search.node.js
@@ -4,42 +4,43 @@ var index = require('../index');
 
 // Find the incoming nodes for a specific node
 var params = {
-  label: ':hotel',
+  linkType: ':FILTERS',
+  maxDept: 5,
   limit: 2
 };
 
-index.search.node('amenity:ne.outdoorpool').incoming(params, function (err, data) {
+index.search.node(['tile:article.geo.spain', 'tile:article.geo.london']).outgoing(params, function (err, data) {
   if (err) console.error(err);
   console.log(JSON.stringify(data, null, 2));
   assert.equal(data.length, 2);
 });
 
-// Find the outgoing nodes for a specific node
-var params2 = {
-  label: ':amenity',
-  limit: 2
-};
+// // Find the outgoing nodes for a specific node
+// var params2 = {
+//   label: ':amenity',
+//   limit: 2
+// };
 
-index.search.node('hotel:ne.wvid.6530').outgoing(params2, function (err, data) {
-  if (err) console.error(err);
-  console.log(JSON.stringify(data, null, 2));
-  assert.equal(data.length, 2);
-});
+// index.search.node('hotel:ne.wvid.6530').outgoing(params2, function (err, data) {
+//   if (err) console.error(err);
+//   console.log(JSON.stringify(data, null, 2));
+//   assert.equal(data.length, 2);
+// });
 
-// Find the in and outgoing nodes for a specific node
-var params3 = {
-  limit: 2
-};
+// // Find the in and outgoing nodes for a specific node
+// var params3 = {
+//   limit: 2
+// };
 
-index.search.node('hotel:ne.wvid.6530').links(params3, function (err, data) {
-  if (err) console.error(err);
-  console.log(JSON.stringify(data, null, 2));
-  assert.equal(data.length, 2);
-});
+// index.search.node('hotel:ne.wvid.6530').links(params3, function (err, data) {
+//   if (err) console.error(err);
+//   console.log(JSON.stringify(data, null, 2));
+//   assert.equal(data.length, 2);
+// });
 
-// Find a specific node
-index.search.node('hotel:ne.wvid.6530').find(function (err, data) {
-  if (err) console.error(err);
-  console.log(JSON.stringify(data, null, 2));
-  assert.equal(data.properties.id, 'hotel:ne.wvid.6530');
-});
+// // Find a specific node
+// index.search.node('hotel:ne.wvid.6530').find(function (err, data) {
+//   if (err) console.error(err);
+//   console.log(JSON.stringify(data, null, 2));
+//   assert.equal(data.properties.id, 'hotel:ne.wvid.6530');
+// });
