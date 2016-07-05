46ad6259a7ac31c3863fe6690c23f1814411b577
Leonard Martin <leonard.martin@gmail.com>
2016-07-05 13:35:07 +0100

    Refactor tests for suggest method to not hit real elasticsearch
    We want to avoid relying on a real elasticsearch instance for unit tests in order to be able to make them faster and more repeatable. Instead create assertions on the queries that are passed into elastic search and the processing of the data it returns based on a mock search client.
    
    Uses a fixture based on the result output by prod elastic earch at time of writing.

---
+++ test/suggest/index.js
@@ -0,0 +1,118 @@
+const simple = require('simple-mock');
+const ElasticSearch = require('elasticsearch');
+const assert = require('assert');
+const uniq = require('lodash.uniq');
+const all = require('lodash.every');
+
+const suggest = require('../../').suggest;
+
+describe('suggest', () => {
+  const mockClient = {
+    search: simple.mock()
+  };
+  beforeEach(() => {
+    mockClient.search.callbackWith(null, require('./fixtures/result.json'));
+    simple.mock(ElasticSearch, 'Client').returnWith(mockClient);
+  });
+  afterEach(() => {
+    simple.restore();
+  });
+
+  it('calls to elasticsearch search method', (done) => {
+    suggest({ text: 'spa' }, () => {
+      assert.equal(mockClient.search.callCount, 1);
+      assert.equal(mockClient.search.lastCall.args[0].index, 'taggy');
+      assert.equal(mockClient.search.lastCall.args[0].type, 'taggy');
+      done();
+    });
+  });
+
+  describe('query', () => {
+    it('passes start and size options to elasticsearch query', (done) => {
+      suggest({ text: 'spa', start: 100, size: 10 }, () => {
+        assert.equal(mockClient.search.lastCall.args[0].from, 100);
+        assert.equal(mockClient.search.lastCall.args[0].size, 10);
+        done();
+      });
+    });
+
+    it('does a prefix match on the name property for simple string searches', (done) => {
+      suggest({ text: 'spa' }, () => {
+        const query = mockClient.search.lastCall.args[0].body.query.filtered.query.bool;
+        assert.deepEqual(query.must[0], { match_phrase_prefix: { name: 'spa' } });
+        done();
+      });
+    });
+    it('does a prefix match on the tagid property if the serch string looks like a tag', (done) => {
+      suggest({ text: 'abc:def.ghi' }, () => {
+        const query = mockClient.search.lastCall.args[0].body.query.filtered.query.bool;
+        assert.deepEqual(query.must[0], { match_phrase_prefix: { tagid: 'abc:def.ghi' } });
+        done();
+      });
+    });
+    it('adds a context match to query if a context option is provided', (done) => {
+      suggest({ text: 'spa', context: 'dk:da' }, () => {
+        const query = mockClient.search.lastCall.args[0].body.query.filtered.query.bool;
+        assert.deepEqual(query.must[1], { match: { context: 'dk:da' } });
+        done();
+      });
+    });
+  });
+
+  describe('results', () => {
+    describe('uniqueness', () => {
+      it('returns each tagid only once', (done) => {
+        suggest({ text: 'spa' }, (err, result) => {
+          assert(!err);
+          const ids = result.data.hits.hit.map(o => o.fields.tagid);
+          assert.equal(ids.length, uniq(ids).length);
+          done();
+        });
+      });
+    });
+    describe('filtering', () => {
+      it('removes any results which match the type provided in `exclude` option', (done) => {
+        suggest({ text: 'spa', exclude: 'hotel' }, (err, result) => {
+          assert(!err);
+          const types = result.data.hits.hit
+            .map(o => o.fields.tagid)
+            .map(id => id.split(':')[0]);
+          assert(all(types, type => type !== 'hotel'));
+          done();
+        });
+      });
+      it('removes any results which do not match the type provided in `include` option', (done) => {
+        suggest({ text: 'spa', include: 'marketing' }, (err, result) => {
+          assert(!err);
+          const types = result.data.hits.hit
+            .map(o => o.fields.tagid)
+            .map(id => id.split(':')[0]);
+          assert(all(types, type => type === 'marketing'));
+          done();
+        });
+      });
+      it('supports an array as `include` option', (done) => {
+        suggest({ text: 'spa', include: ['marketing', 'geo'] }, (err, result) => {
+          assert(!err);
+          const types = result.data.hits.hit
+            .map(o => o.fields.tagid)
+            .map(id => id.split(':')[0]);
+          assert(all(types, type => (type === 'marketing' || type === 'geo')));
+          done();
+        });
+      });
+    });
+  });
+
+  describe('error handling', () => {
+    it('calls callback with error if elasticsearch fails', (done) => {
+      mockClient.search.actions = [];
+      mockClient.search.callbackWith(new Error('elasticsearch error'));
+      suggest({ text: 'spa' }, (err) => {
+        assert(err);
+        assert.equal(err.message, 'elasticsearch error');
+        done();
+      });
+    });
+  });
+});
