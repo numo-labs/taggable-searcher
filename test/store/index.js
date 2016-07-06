bed9551a0e923fe106425bd115d3ba62f809d417
Leonard Martin <leonard.martin@gmail.com>
2016-07-06 10:22:58 +0100

    Fix bug with init not configuring s3 parameters
    The bucket name and environment were never set when passed to `init` as parameters due to a broken conditional statement.
    
    Also refactors tests to ensure proper assertions are carried out on the code.

---
+++ test/store/index.js
@@ -0,0 +1,78 @@
+const simple = require('simple-mock');
+const AWS = require('aws-sdk');
+const assert = require('assert');
+const store = require('../../').store;
+
+describe('store', () => {
+  const mockResult = { Body: { toString: simple.mock().returnWith('test-string') } };
+  beforeEach(() => {
+    mockResult.Body.toString.reset();
+    simple.mock(AWS.S3.prototype, 'getObject').callbackWith(null, mockResult);
+    store.init('numo-taggy', 'ci');
+  });
+  afterEach(() => {
+    simple.restore();
+  });
+  it('calls through to S3.getObject', (done) => {
+    store.get('geo:geonames.10062607', () => {
+      assert.equal(AWS.S3.prototype.getObject.callCount, 1);
+      done();
+    });
+  });
+  it('loads from "numo-taggy" bucket', (done) => {
+    store.get('geo:geonames.10062607', () => {
+      const params = AWS.S3.prototype.getObject.lastCall.args[0];
+      assert.equal(params.Bucket, 'numo-taggy');
+      done();
+    });
+  });
+  it('calls callback with `toString` from result Body', (done) => {
+    store.get('geo:geonames.10062607', (err, result) => {
+      assert(!err);
+      assert.equal(mockResult.Body.toString.callCount, 1);
+      assert.equal(result, 'test-string');
+      done();
+    });
+  });
+  describe('object key generation', () => {
+    it('geo:geonames.10062607', (done) => {
+      store.get('geo:geonames.10062607', () => {
+        const params = AWS.S3.prototype.getObject.lastCall.args[0];
+        assert.equal(params.Key, 'ci/geo/geonames/geo:geonames.10062607.json');
+        done();
+      });
+    });
+    it('marketing:term.spa', (done) => {
+      store.get('marketing:term.spa', () => {
+        const params = AWS.S3.prototype.getObject.lastCall.args[0];
+        assert.equal(params.Key, 'ci/marketing/term/marketing:term.spa.json');
+        done();
+      });
+    });
+    it('amenity:ne.wifi', (done) => {
+      store.get('amenity:ne.wifi', () => {
+        const params = AWS.S3.prototype.getObject.lastCall.args[0];
+        assert.equal(params.Key, 'ci/amenity/ne/amenity:ne.wifi.json');
+        done();
+      });
+    });
+  });
+  describe('init', () => {
+    it('sets the environment if passed as a parameter', (done) => {
+      store.init(null, 'prod');
+      store.get('geo:geonames.10062607', () => {
+        const params = AWS.S3.prototype.getObject.lastCall.args[0];
+        assert.equal(params.Key, 'prod/geo/geonames/geo:geonames.10062607.json');
+        done();
+      });
+    });
+    it('sets the bucket if passed as a parameter', (done) => {
+      store.init('test-bucket');
+      store.get('geo:geonames.10062607', () => {
+        const params = AWS.S3.prototype.getObject.lastCall.args[0];
+        assert.equal(params.Bucket, 'test-bucket');
+        done();
+      });
+    });
+  });
+});
