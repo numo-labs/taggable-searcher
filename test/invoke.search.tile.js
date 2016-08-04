df3ba39791182ab676d2f56483415c55f346fdae
Marcelo Gomez <gomezconesa.marcelo@gmail.com>
2016-08-04 16:58:15 +0200

    include tiles that are included in tag filters

--- test/invoke.search.tile.js
+++ test/invoke.search.tile.js
@@ -29,4 +29,16 @@ describe('Integration: search.tile', function () {
       done();
     });
   });
+  it('should find results for Svømmeskole på Sunwing', function (done) {
+    this.timeout(4000);
+    var params = {
+      includedIn: ['tile:article.dk.121'],
+      fastQuery: true
+    };
+    index.search.tile(params, function (err, result) {
+      if (err) return done(err);
+      assert(result.data.length > 0);
+      done();
+    });
+  });
 });
